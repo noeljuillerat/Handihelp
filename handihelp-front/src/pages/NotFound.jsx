add42d2883e49579f55d6e7a6082e7b28c8645e3
noeljuillerat <noel.juillerat@gmail.com>
2021-10-07 12:32:49 +0200

    404

--- handihelp-front/src/pages/NotFound.jsx
+++ handihelp-front/src/pages/NotFound.jsx
@@ -4,25 +4,29 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
         <div className="container">
-    <div className="row">
-        <div className="col-md-12">
-            <div className="error-template">
+    <div className="row align-items-center">
+        <div className="col-md-6 ">
+                    
+            <div className="error-template align-items-center">             
                 <h1>
                     Oops!</h1>
                 <h2>
-                     ERREUR 404 </h2>
+                    ERREUR 404 </h2>
                 <div className="error-details">
                     Désolé, une erreur s'est produite, la page demandée n'a pas été trouvé !
-                </div>
-                        <div claclassNamess="error-actions">
-                        <Link to={`/`}>
-                            <span class="glyphicon glyphicon-home" className="btn btn-primary btn-lg">Retour à l'accueil</span>
-                        </Link>
-                        <Link to={'/contact'} className="btn btn-default btn-lg">
-                        Contacter le support<span className="glyphicon glyphicon-envelope"></span>
-                        </Link>
-                </div>
             </div>
+            <div classNames="error-actions">
+                <Link to={`/`}>
+                    <span class="glyphicon glyphicon-home" className="btn btn-primary btn-lg mt-5">Retour à l'accueil</span>
+                </Link>
+                <Link to={'/contact'} className="btn btn-default btn-lg mt-5">
+                Contacter le support<span className="glyphicon glyphicon-envelope"></span>
+                </Link>
+            </div>
+            </div>
+        </div>
+        <div className="col-md-6">
+            <img src="fauteuil.jpg" alt="404"/>
         </div>
     </div>
 </div>
