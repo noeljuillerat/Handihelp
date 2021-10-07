578a01550a2a30cd49dfd8cc4fa225d0ad36f625
tor <victor.matheron@gmail.com>
2021-10-07 14:12:16 +0200

    Wording missions

--- handihelp-back/src/main/java/epsi/workshop/handihelp/MainService.java
+++ handihelp-back/src/main/java/epsi/workshop/handihelp/MainService.java
@@ -73,15 +73,15 @@ public class MainService {
 
         // USER 1
         User user1 = new User();
-        user1.setNom("Matheron");
-        user1.setPrenom("Victor");
+        user1.setNom("Sylvie");
+        user1.setPrenom("Dubois");
         user1.setRoleUser(roleUser1);
         userService.saveUser(user1);
 
         // USER 2
         User user2 = new User();
-        user2.setNom("Toto");
-        user2.setPrenom("Titi");
+        user2.setNom("Julien");
+        user2.setPrenom("Laville");
         user2.setRoleUser(roleUser2);
         userService.saveUser(user2);
 
@@ -89,46 +89,46 @@ public class MainService {
         Mission mission1 = new Mission();
         mission1.setCreateur(user1);
         mission1.setBenevole(user2);
-        mission1.setTitre("Mission 1");
+        mission1.setTitre("Prise de notes");
         mission1.setDateHeure("Le 07 Octobre 2021 à 14h00");
         mission1.setDuree("1H");
         mission1.setLieu("Cours de C#");
         mission1.setEtablissement(etablissement1);
-        mission1.setDescription("Une super mission à faire pour rendre service à quelqu'un");
+        mission1.setDescription("J'ai besoin d'aide pour la prise de notes pendant le cours, car j'ai des problèmes d'audition. Merci d'avance !");
         mission1.setStatutMission(statutMission3);
         missionService.saveMission(mission1);
 
         Mission mission2 = new Mission();
         mission2.setCreateur(user1);
         mission2.setBenevole(user2);
-        mission2.setTitre("Mission 2");
+        mission2.setTitre("Déjeuner");
         mission2.setDateHeure("Le 15 Octobre 2021 à 10h30");
         mission2.setDuree("30min");
         mission2.setLieu("Locaux de l'EPSI");
         mission2.setEtablissement(etablissement1);
-        mission2.setDescription("Une deuxième super mission à faire pour rendre service à quelqu'un");
+        mission2.setDescription("Est-ce que quelqu'un pourrait me rendre service et aller me chercher quelque chose à la boulangerie ce jour là. Je n'aurais pas le temps de préparer quelque chose à manger et j'ai des problèmes pour me déplacer.");
         mission2.setStatutMission(statutMission2);
         missionService.saveMission(mission2);
 
         Mission mission3 = new Mission();
         mission3.setCreateur(user1);
-        mission3.setTitre("Mission 3");
+        mission3.setTitre("Covoiturage");
         mission3.setDateHeure("Le 22 Octobre 2021 à 8H00");
         mission3.setDuree("30min");
         mission3.setLieu("Trajet domicile / centre de formation");
         mission3.setEtablissement(etablissement2);
-        mission3.setDescription("Une troisième super mission à faire pour rendre service à quelqu'un");
+        mission3.setDescription("Je n'aurais personne pour m'emmener sur le lieu de formation ce jour là. Je cherche donc un covoiturage.");
         mission3.setStatutMission(statutMission1);
         missionService.saveMission(mission3);
 
         Mission mission4 = new Mission();
         mission4.setCreateur(user1);
-        mission4.setTitre("Mission 4");
+        mission4.setTitre("Aide révisions");
         mission4.setDateHeure("Le 22 Octobre 2021 à 12H00");
         mission4.setDuree("30min");
-        mission4.setLieu("Caffeteria");
+        mission4.setLieu("Bibliothèque");
         mission4.setEtablissement(etablissement2);
-        mission4.setDescription("Une quatrième super mission à faire pour rendre service à quelqu'un");
+        mission4.setDescription("J'ai des problèmes d'attention et souhaiterais pouvoir réviser avec quelqu'un qui puisse m'expliquer les concepts vus en cours. Merci pour votre aide.");
         mission4.setStatutMission(statutMission1);
         missionService.saveMission(mission4);
 
